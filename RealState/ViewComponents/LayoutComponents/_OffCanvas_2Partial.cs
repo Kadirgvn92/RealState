1bb1a2e1c3c8880903e6e9a51ea0105bc61b9097
Kadirgvn92 <kdrgvndeveloper@gmail.com>
2024-07-06 09:19:56 +0300

    Started to design admin panel

---
+++ RealState/ViewComponents/LayoutComponents/_OffCanvas_2Partial.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RealState.ViewComponents.LayoutComponents;
+
+public class _OffCanvas_2Partial : ViewComponent
+{
+    public IViewComponentResult Invoke()
+    {
+        return View();
+    }
+}
