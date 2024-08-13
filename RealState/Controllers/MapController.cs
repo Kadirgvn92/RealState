270b3a76a91946139a9c175f3579fe1fab9e2708
Kadirgvn92 <kdrgvndeveloper@gmail.com>
2024-08-13 03:21:42 +0300

    MapController created and map showed successfully

---
+++ RealState/Controllers/MapController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RealState.Controllers;
+public class MapController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
