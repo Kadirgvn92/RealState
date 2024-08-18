063b7d802b14e0daedb1a9b25c5a986e963191d1
Kadirgvn92 <kdrgvndeveloper@gmail.com>
2024-08-18 18:20:53 +0300

    Calendar created but cannot show

---
+++ RealState/Controllers/CalendarController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RealState.Controllers;
+public class CalendarController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
